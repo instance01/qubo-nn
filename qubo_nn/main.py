d1d8e256ac012819502a3e02723f905f1f751205
instance01 <nibbadont@protonmail.com>
2021-02-17 10:21:11 +0100

    Change command line interface

--- qubo_nn/main.py
+++ qubo_nn/main.py
@@ -4,19 +4,23 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("type")
-parser.add_argument("cmd")
-parser.add_argument("cfg_id")
-parser.add_argument("--model", nargs="?")
+parser.add_argument("-t", "--type", nargs=None, help="Type (classify)")
+# parser.add_argument("--cmd", nargs=1, help="Command (gendata, train, eval)")
+parser.add_argument("--eval", action="store_true")
+parser.add_argument("--gendata", action="store_true")
+parser.add_argument("--train", action="store_true")
+parser.add_argument("-c", "--cfg_id", nargs=None, help="cfg_id")
+parser.add_argument("-m", "--model", nargs="?")
 args = parser.parse_args()
+
+# TODO: Some optionals are 'too optional' - i.e. I use them but they're optional
 
 cfg = Config().get_cfg(args.cfg_id)
 
 if args.type == 'classify':
-    print(args.cmd)
-    if args.cmd == 'gendata':
+    if args.gendata:
         Classification(cfg).prep_data()
-    elif args.cmd == 'train':
+    elif args.train:
         Classification(cfg).run_experiment()
-    elif args.cmd == 'eval':
+    elif args.eval:
         Classification(cfg).eval(args.model)
