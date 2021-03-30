43ad43b8ec1941ab7eee7c8c27a9a572b2630dac
InstanceLabs <nibbadont@protonmail.com>
2021-03-30 11:03:25 +0200

    Add some duplicate checking to M3SAT.

--- qubo_nn/problems/max3sat.py
+++ qubo_nn/problems/max3sat.py
@@ -67,6 +67,10 @@ class Max3SAT(Problem):
 
     @classmethod
     def gen_problems(self, cfg, n_problems, size=(20, 25), **kwargs):
+        check_duplicates = cfg["problems"]["M3SAT"].get("check_duplicates", False)
+        if check_duplicates:
+            print("M3SAT gen_problems: Checking for duplicates.")
+
         # size: First is n_vars, second is number of clauses.
         # Want something like this:
         # [((1, True), (2, True), (3, True)), ((1, True), (2, False), (4, False))]
@@ -74,15 +78,58 @@ class Max3SAT(Problem):
         problems = []
         for _ in range(n_problems):
             problem = []
+
             # Eg: 25 clauses
             for _ in range(size[1]):
                 # Each has THREE tuples!! But random vars and random value.
                 # (True/False).
+                idx1 = int(round(random.random() * n_vars))
+                idx2 = int(round(random.random() * n_vars))
+                idx3 = int(round(random.random() * n_vars))
+                val1 = bool(random.getrandbits(1))
+                val2 = bool(random.getrandbits(1))
+                val3 = bool(random.getrandbits(1))
+
+                # TODO: With the checks below we sometimes end up with less than the desired # of clauses.
+
+                if check_duplicates:
+                    if idx1 == idx2 or idx1 == idx3 or idx2 == idx3:
+                        continue
+
                 clause = (
-                    (int(round(random.random() * n_vars)), bool(random.getrandbits(1))),
-                    (int(round(random.random() * n_vars)), bool(random.getrandbits(1))),
-                    (int(round(random.random() * n_vars)), bool(random.getrandbits(1)))
+                    (abs(idx1), val1),
+                    (abs(idx2), val2),
+                    (abs(idx3), val3)
                 )
-                problem.append(sorted(clause))
-            problems.append(sorted(problem))
+                clause = sorted(clause)
+
+                if check_duplicates:
+                    if clause in problem:
+                        continue
+
+                    other_clauses = [
+                        [clause[0], (clause[1][0], not clause[1][1]), clause[2]],
+                        [clause[0], (clause[1][0], not clause[1][1]), (clause[2][0], not clause[2][1])],
+                        [clause[0], clause[1], (clause[2][0], not clause[2][1])],
+                        [(clause[0], not clause[0][1]), (clause[1][0], not clause[1][1]), clause[2]],
+                        [(clause[0], not clause[0][1]), (clause[1][0], not clause[1][1]), (clause[2][0], not clause[2][1])],
+                        [(clause[0], not clause[0][1]), clause[1], (clause[2][0], not clause[2][1])],
+                    ]
+                    do_cont = False
+                    for other_clause in other_clauses:
+                        if other_clause in problem:
+                            do_cont = True
+                    if do_cont:
+                        continue
+
+                problem.append(clause)
+            if len(problem) == size[1]:
+                problems.append(sorted(problem))
+        len_ = len(problems)
+
+        # # TODO Suboptimal solution tbh fam.
+        # if len_ < n_problems:
+        #     problems = np.array(problems)
+        #     problems = problems.repeat(n_problems // len_ + 1, axis=0)
+        #     problems = problems[:-(len_ - n_problems % len_)]
         return [{"clauses": problem, "n_vars": size[0]} for problem in problems]
