ff90002d8db668ad0eb00b339c83715c0233fa43
instance01 <nibbadont@protonmail.com>
2021-04-03 18:45:43 +0200

    Only use 50 data points

--- qubo_nn/plots/plot_qa_best_99_example.py
+++ qubo_nn/plots/plot_qa_best_99_example.py
@@ -15,15 +15,20 @@ x = [1.97, 7.48, 0.8, 8.42, 0.58, 0.79, 1.63, 3.94, 4.02, 3.76, 8.7, 9.55, 9.88,
 y = [2.04, 7.35, 0.82, 8.16, 0.61, 0.82, 1.43, 3.88, 4.08, 3.67, 8.57, 9.59, 9.8, 7.14, 8.98, 7.14, 6.12, 4.29, 2.86, 8.57, 4.49, 7.14, 0.82, 3.06, 4.9, 8.98, 3.67, 9.18, 5.1, 5.92, 8.57, 8.78, 4.49, 5.51, 3.88, 9.39, 4.9, 2.86, 2.45, 2.45, 4.08, 4.69, 4.9, 1.22, 7.96, 8.57, 1.43, 7.76, 9.8, 1.02, 0.41, 9.8, 3.47, 5.1, 6.53, 9.59, 7.35, 2.45, 2.24, 4.49, 9.59, 4.29, 1.84, 9.8, 8.57, 7.55, 2.04, 7.76, 8.98, 7.55, 7.76, 0.2, 10.0, 6.73, 2.24, 9.8, 7.76, 1.02, 8.78, 1.63, 1.02, 2.24, 8.16, 7.14, 1.02, 2.24, 7.96, 5.71, 7.96, 4.08, 3.88, 7.55, 0.82, 8.98, 2.65, 5.92, 6.53, 6.12, 3.47, 3.67, 0.61, 3.27, 7.76, 2.65, 8.16, 3.88, 4.69, 3.06, 3.06, 9.59, 10.0, 7.14, 1.43, 0.61, 8.57, 8.16, 9.8, 7.76, 3.27, 9.18, 0.2, 1.22, 6.73, 8.98, 5.71, 8.16, 3.67, 9.18]  # noqa
 
 
+# 128 data points is too much to visualize side by side.
+x = x[:50]
+y = y[:50]
+
+
 idx = np.arange(len(y))
 width = np.min(np.diff(idx)) / 3
 
-fig = plt.figure(figsize=(15, 3))
+fig = plt.figure(figsize=(6, 2))
 ax = fig.add_subplot(111)
-ax.bar(idx - width, x, width, label="Predicted")
-ax.bar(idx, y, width, label="Truth")
+ax.bar(idx - width + .25, x, width, label="Predicted")
+ax.bar(idx + .25, y, width, label="Truth")
 plt.ylim(0)
-plt.xlim(0, 128)
+plt.xlim(-1, 50)
 plt.legend()
 plt.ylabel("Value")
 plt.xlabel("QA Parameter")
@@ -32,10 +37,10 @@ fig.savefig('qa_best_99_value.png')
 fig.savefig('qa_best_99_value.pdf')
 plt.show()
 
-fig = plt.figure(figsize=(15, 3))
+fig = plt.figure(figsize=(6, 2))
 diff = np.array(x) - np.array(y)
 plt.bar(idx, diff, width)
-plt.xlim(0, 128)
+plt.xlim(-1, 50)
 plt.ylabel("Residual")
 plt.xlabel("QA Parameter")
 plt.tight_layout()
