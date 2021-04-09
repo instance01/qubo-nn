24ed6cfd0bd344c7f13c286ae212cee24e28df50
InstanceLabs <nibbadont@protonmail.com>
2021-04-09 16:45:55 +0200

    Add gen_sp_vars.py and generated SP pickle.

---
+++ qubo_nn/plots/gen_sp_vars.py
@@ -0,0 +1,35 @@
+import glob
+import pickle
+
+from lib import aggregate_single
+
+
+def aggregate(paths, min_len, cutoff=False):
+    arr_eval = aggregate_single(paths, 'Loss/Eval', min_len)
+    arr_train = aggregate_single(paths, 'Loss/Train', min_len)
+    if cutoff:
+        arr_r2 = aggregate_single(paths, 'Custom/R2', min_len, cutoff=6)
+    else:
+        arr_r2 = aggregate_single(paths, 'Custom/R2', min_len)
+    return arr_eval, arr_train, arr_r2
+
+
+def run():
+    min_len = 100
+    base_path = '../runs/'
+    problems = ['sp4', 'sp5', 'sp6', 'sp4_sort', 'sp5_sort', 'sp6_sort']
+    kv = {}
+    for problem in problems:
+        print(problem)
+        paths = glob.glob(base_path + '*-' + problem)
+
+        print(paths)
+        data = aggregate(paths, min_len, False)
+        kv[problem] = data
+
+    with open('sp_vars.pickle', 'wb+') as f:
+        pickle.dump(kv, f)
+
+
+if __name__ == '__main__':
+    run()
