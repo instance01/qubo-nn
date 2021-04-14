d282dab756521b6d327d7df521a575ee2264e103
InstanceLabs <nibbadont@protonmail.com>
2021-04-14 08:06:22 +0200

    Update QA comp generator with new special loss.

--- qubo_nn/plots/gen_qa_comp.py
+++ qubo_nn/plots/gen_qa_comp.py
@@ -7,7 +7,8 @@ from lib import aggregate_single
 def aggregate(paths, min_len, len_, req_len=None):
     fpfn_tot_ratio = aggregate_single(paths, 'Custom/FPFN_TOT_Ratio', min_len, req_len=req_len)
     r2 = aggregate_single(paths, 'Custom/R2', min_len, req_len=req_len)
-    return fpfn_tot_ratio, r2
+    eval_loss = aggregate_single(paths, 'Loss/Eval', min_len, req_len=req_len)
+    return fpfn_tot_ratio, r2, eval_loss
 
 
 def run():
@@ -28,7 +29,9 @@ def run():
         ('qa_N_144_norm3', 10),
 
         ('qa_N_16_norm2_1M', 10),
-        ('qa_N_16_norm2_4M', 10)
+        ('qa_N_16_norm2_4M', 10),
+
+        ('qa_special_loss1', 10)
     ]
     kv = {}
     for problem in problems:
