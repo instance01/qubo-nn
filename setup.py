a8382b4be7376e7c66547733165686e25acba24b
InstanceLabs <nibbadont@protonmail.com>
2021-06-07 22:45:08 +0200

    Bump version to 0.2.0. This is symbolic.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='qubo-nn',
-    version='0.1.0',
+    version='0.2.0',
     install_requires=[
         'numpy', 'networkx', 'torchvision', 'torch', 'dwave-qbsolv'
     ]
