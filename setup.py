557926738f5b0c1b39cea17829120622a51e7050
instance01 <nibbadont@protonmail.com>
2021-07-01 11:34:57 +0200

    Finally finish Pypi integration.

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='qubo-nn',
-    version='0.2.3',
+    version='0.2.5',
     install_requires=[
         'numpy', 'networkx', 'torchvision', 'torch', 'dwave-qbsolv',
         'qubovert', 'matplotlib', 'scipy', 'tensorflow', 'sklearn',
-        'tensorboard',
-        'ml-pyxis@git+https://github.com/vicolab/ml-pyxis@master',
+        'tensorboard', 'ml-pyxis',
     ],
     long_description_content_type='text/markdown',
-    description='QUBO translations for 14 problems. Also: Reverse-engeering and AutoEncoders for QUBOs.',
+    description='QUBO translations for 14 problems. Also: Reverse-engeering '
+                'and AutoEncoders for QUBOs.',
     url='https://github.com/instance01/qubo-nn',
     author='Instance01',
     author_email='whodis@instancedev.com',
     license='MIT',
-    packages=['qubo_nn'],
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
